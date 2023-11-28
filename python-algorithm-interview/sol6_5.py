7dd53f1c9ced6fe79dbe0c8972aa945c52b1d6cd
jonghun Park <jhp2014.dev@gmail.com>
2023-11-28 11:27:01 +0900

    leetcode 5번
    - 더 좋은 솔루션이다.

--- python-algorithm-interview/sol6_5.py
+++ python-algorithm-interview/sol6_5.py
@@ -1,33 +1,25 @@
-# 비효율 적인 풀이다.
-
+'''
+올바른 솔루션
+나는 "ab"가 입력인 경우의 엣지 케이스를 제대로 처리하지 못했다.
+생각도 못했었다.
+'''
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left+1: right]
+        
         if len(s) < 2 or s == s[::-1]:
             return s
         
         result = ""
-        for idx in range(0, len(s)):
-            result = max(self.startTwoWords(s, idx), result, key=len)
-            result = max(self.startTreeWords(s, idx), result, key=len)
-        
+        for i in range(len(s) - 1):
+            result = max(result,
+                         expand(i, i+1),
+                         expand(i, i+2),
+                         key=len)
+            
         return result
-    
-    def startTwoWords(self, s, idx):
-        left = idx
-        right = idx + 1
-        
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-
-        return s[left + 1: right]
-
-    def startTreeWords(self, s, idx):
-        left = idx
-        right = idx + 2
-
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-        
-        return s[left + 1: right]
